3c06fb9aa1cb8eb791884579c8667e4cbbb86c1d
Sophia Haines <sophia.haines@gmail.com>
2015-06-02 15:03:38 +0800

    init commit

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  })
+
+  $("select").change(function(){
+    $( "select option:selected").each(function(){
+      if($(this).attr("value")=="contact"){
+        $(".box").not(".contact").hide();
+        $(".contact").show();
+      }
+      else if($(this).attr("value")=="trial"){
+        $(".box").not(".trial").hide();
+        $(".trial").show();
+      }
+      else if($(this).attr("value")=="newsletter"){
+        $(".box").not(".newsletter").hide();
+        $(".newsletter").show();
+      }
+      else{
+        $(".box").show();
+      }
+    });
+  }).change();
+});
