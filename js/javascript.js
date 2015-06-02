60ce56c96e1cc92f72bf89327571ed26293735c3
Sophia Haines <sophia.haines@gmail.com>
2015-06-02 18:01:41 +0800

    fade in and out of different tables

--- js/javascript.js
+++ js/javascript.js
@@ -3,26 +3,28 @@ $(document).ready(function(){
     $('#myInput').focus()
   })
 
+  $(".contact").hide();
+  $(".trial").hide();
+  $(".newsletter").hide();
+
   $("select").change(function(){
     $( "select option:selected").each(function(){
       if($(this).attr("value")=="contact"){
-        $(".box").not(".contact").hide();
-        $(".contact").show();
+        $(".box").not(".contact").fadeOut(500);
+        $(".contact").delay(500).fadeIn(500);
       }
       else if($(this).attr("value")=="trial"){
-        $(".box").not(".trial").hide();
-        $(".trial").show();
+        $(".box").not(".trial").fadeOut(500);
+        $(".trial").delay(500).fadeIn(500);
       }
       else if($(this).attr("value")=="newsletter"){
-        $(".box").not(".newsletter").hide();
-        $(".newsletter").show();
+        $(".box").not(".newsletter").fadeOut(500);
+        $(".newsletter").delay(500).fadeIn(500);
       }
       else{
-        $(".box").not(".newsletter").hide();
-        $(".box").not(".contact").hide();
-        $(".box").not(".trial").hide();
-        $(".demo").show();
+        $(".box").not(".demo").fadeOut(500);
+        $(".demo").delay(500).fadeIn(500);
       }
     });
-  }).change();
+  })
 });
