302aaf03af8a8463af1f3fb91c8d1afcb38fec41
simon245 <simonball245@gmail.com>
2015-06-30 13:23:46 +0800

    added posts for each form. added require for json and sinatra-reloader

--- main.rb
+++ main.rb
@@ -1,9 +1,33 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+$stdout.sync = true
 
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
-get '/contact_form' do
-  # TODO
+post '/demo', :provides => :json do
+  params = JSON.parse(request.body.read)
+  puts params
+  {}
+end
+
+post '/contact', :provides => :json do
+  params = JSON.parse(request.body.read)
+  puts params
+  {}
+end
+
+post '/trial', :provides => :json do
+  params = JSON.parse(request.body.read)
+  puts params
+  {}
+end
+
+post '/newsletter', :provides => :json do
+  params = JSON.parse(request.body.read)
+  puts params
+  {}
 end
