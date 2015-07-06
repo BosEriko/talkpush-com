ea4b0f3ef1a7119bb9326397bb57dd35450cb05d
Eato Lau <eato.lau@talkpush.com>
2015-07-06 16:32:09 +0800

    - Changed the Video links - Added Message box for form - Added Timeline in company page

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -38,19 +38,19 @@ $(document).ready(function(){
   $('.btnViewPL').click(function(){scrollTo('#price');});
   $('.btnStartup').click(function(){
       scrollTo('.form-container');
-      $('#message').val('I would like to schedule a demo of startup plan. '+$('#message').val());
+      $('#message').val('I would like to schedule a demo of startup plan. ');
   });
   $('.btnSmallBiz').click(function(){
       scrollTo('.form-container');
-      $('#message').val('I would like to schedule a demo of small business plan. '+$('#message').val());
+      $('#message').val('I would like to schedule a demo of small business plan. ');
   });
   $('.btnEnterprise').click(function(){
       scrollTo('.form-container');
-      $('#message').val('I would like to schedule a demo of enterprise plan. '+$('#message').val());
+      $('#message').val('I would like to schedule a demo of enterprise plan. ');
   });
   $('.btnCustom').click(function(){
       scrollTo('.form-container');
-      $('#message').val('I would like to schedule a demo of custom plan. '+$('#message').val());
+      $('#message').val('I would like to know more detail of custom plan. ');
   });
 
   function scrollTo(sectionClass){
@@ -64,12 +64,17 @@ $('.apply4SE').on('click',function(){
     formStr='<div class="wrap form-wrap"><div class="container form-container"><div class="row"><div class="col-md-8 col-md-offset-2 col-xs-12 interested-in"><h2>Apply for '+positionName+'</h2></div><div class="contact-form box"><div class="row"><div class="col-md-8 .col-xs-12 col-md-offset-2"><form class="job-form box" action="/job-form" method="POST"><div class="form-group"><input type="text" class="form-control" id="fullName" placeholder=" NAME"></div><div class="form-group"><input type="email" class="form-control" id="email" placeholder=" E-MAIL"></div><div class="form-group"><div class="form-control" id="cv" placeholder=" CV"><i class="fa fa-file-text"></i>&nbsp;&nbsp;UPLOAD YOUR CV</div></div><div class="form-group"><textarea rows="5" class="form-control" id="message" placeholder=" COVER LETTER"></textarea></div><div class="form-group"><div class="col-sm-offset-"><button type="submit" class="btn btn-form">Submit</button></div></div></form></div></div></div></div></div></div>';
     $('.form-SE').append(formStr);
     scrollTo(".form-SE");
+    $('#cv').click(function(){alert("UPLOAD HERE B")});
 });
+    $(".btnTrial").click(function(){
+        scrollTo(".form-container");
+    });
 $('.apply4AM').on('click',function(){
     positionName="Account Manager";
     formStr='<div class="wrap form-wrap"><div class="container form-container"><div class="row"><div class="col-md-8 col-md-offset-2 col-xs-12 interested-in"><h2>Apply for '+positionName+'</h2></div><div class="contact-form box"><div class="row"><div class="col-md-8 .col-xs-12 col-md-offset-2"><form class="job-form box" action="/job-form" method="POST"><div class="form-group"><input type="text" class="form-control" id="fullName" placeholder=" NAME"></div><div class="form-group"><input type="email" class="form-control" id="email" placeholder=" E-MAIL"></div><div class="form-group"><div class="form-control" id="cv" placeholder=" CV"><i class="fa fa-file-text"></i>&nbsp;&nbsp;UPLOAD YOUR CV</div></div><div class="form-group"><textarea rows="5" class="form-control" id="message" placeholder=" COVER LETTER"></textarea></div><div class="form-group"><div class="col-sm-offset-"><button type="submit" class="btn btn-form">Submit</button></div></div></form></div></div></div></div></div></div>';
     $('.form-AM').append(formStr);
     scrollTo(".form-AM");
+    $('#cv').click(function(){alert("UPLOAD HERE")});
 });
   $('form').on('submit', function(event) {
     event.preventDefault();
@@ -82,7 +87,6 @@ $('.apply4AM').on('click',function(){
       company: $("#company").val(),
       message: $("#message").val()
     }
-
     $.ajax({
       url: form.attr('action'),
       dataType: 'json',
@@ -91,8 +95,13 @@ $('.apply4AM').on('click',function(){
       data : JSON.stringify(data),
       accepts: "application/json",
       error: function(req, err){
-        console.log('error message: ' + err); },
+        console.log('error message: ' + err); 
+        $(".form-message-box").html(err);
+        $(".form-message-box").animate({"opacity":"1"},"slow").animate({"opacity":"0"},2000);
+      },
       success: function(json) {
+        $(".form-message-box").html("Successful!");
+        $(".form-message-box").animate({"opacity":"1"},"slow").animate({"opacity":"0"},2000);
       }
     })
   });
