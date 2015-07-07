64e5abeb67e0ed231a688a53831f45311627a61c
simon245 <simonball245@gmail.com>
2015-07-07 16:37:04 +0800

    added pony.mail for job-form application

--- main.rb
+++ main.rb
@@ -30,11 +30,28 @@ post '/contact-form', :provides => :json do
       :domain         => ENV["DOMAIN"]
     }
   })
+  puts params
 end
 
 post '/job-form', :provides => :json do
   params = JSON.parse(request.body.read)
-  puts "job application"
+  Pony.mail({
+  :to => ENV["TO_ADDRESS"],
+  :via => :smtp,
+  :from => ENV["EMAIL_ADDRESS"],
+  :subject => "Application for #{params["position"]}",
+  # :headers => { 'Content-Type' => 'text/html' }, #for adding html emails into the body field
+  :body => params["fullName"] + " has applied for the position of " + params["position"] + "\n" + params["email"] + "\n\n" + params["coverLetter"],
+  :via_options => {
+    :address        => 'smtp.gmail.com',
+    :port           => '25',
+    :user_name      => ENV["EMAIL_ADDRESS"],
+    :password       => ENV["EMAIL_PASSWORD"],
+    :authentication => :plain,
+    :domain         => ENV["DOMAIN"]
+    }
+  })
   puts params
-
 end
+
+# "#{params["fullName"]} has applied for the position of #{params"position"} \n Email address: #{params["email"]} \n #{params["coverLetter"]}",
