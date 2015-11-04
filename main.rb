0feda9b2343b84220e4723655cf0fb8482053d7a
Eato Lau <eato.lau@talkpush.com>
2015-11-04 03:32:57 +0000

    update the get route (main.rb)

--- main.rb
+++ main.rb
@@ -24,9 +24,11 @@ end
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
+
 get '/xmaspromotion' do
   File.read(File.join('public/xmaspromotion', 'index.html'))
 end
+
 post '/contact-form', :provides => :json do
   params = JSON.parse(request.body.read)
 
