05b893d82be4f6fef3c6d1009e53616634d2dd4b
eato.lau@talkpush.com <eato.lau@talkpush.com>
2016-08-17 11:24:35 +0800

    Update accroding to Max commmment

--- public/free-demo/js/javascript.js
+++ public/free-demo/js/javascript.js
@@ -1,11 +1,14 @@
 $(function () {
+  $(".cancel").on("click",function(){
+    $(".thankyou").hide();
+  })
   $('form').parsley().on('field:validated', function() {
     var ok = $('.parsley-error').length === 0;
     $('.bs-callout-info').toggleClass('hidden', !ok);
     $('.bs-callout-warning').toggleClass('hidden', ok);
   })
   .on('form:submit', function() {
-    $(".loadingBlockStyle").show();
+    $(".loading").show();
     var data = {
       fullName: $("input[name='first_name']").val()+" "+$("input[name='last_name']").val(),
       email: $("input[name='email']").val(),
@@ -20,14 +23,15 @@ $(function () {
       data : JSON.stringify(data),
       accepts: "application/json",
       error: function(req, err){
-        $(".loadingBlockStyle").hide();
+        $(".loading").hide();
         console.log('error message: ' + err);
-        $(".form-message-box").html(err +" occurred! <br>Please send a email to <a href='mailto:hello@talkpush.com'>hello@talkpush.com</a>.");
+        $(".form-message-box").html("Error occurred! <br>Please send a email to <a href='mailto:hello@talkpush.com'>hello@talkpush.com</a>.");
         $(".form-message-box").animate({"opacity":"1","color":"red"},"slow").delay(15000).animate({"opacity":"0"},2000);
       },
       success: function(json) {
-        $(".loadingBlockStyle").hide();
-        $("form")[0].reset()
+        $(".loading").hide();
+        $(".thankyou").show();
+        $("form")[0].reset();
         $(".form-message-box").html("Request Sent! We will get back to you as soon as possible.");
         $(".form-message-box").animate({"opacity":"1"},"slow").delay(15000).animate({"opacity":"0"},2000);      
       }
