8b902a5784935c9f9753ea392bdad52bde9bfc68
Bos Eriko <eriko.bos@talkpush.com>
2017-06-29 10:59:12 +0800

    Concatinated the script and style files; and reorganized the files on the root of public

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,35 @@ module.exports = function(grunt){
 
     grunt.initConfig({
       concat: {
-        dist: {
+        js: {
           src: ['./public/scripts/jquery-3.1.0.min.js',
                 './public/scripts/app.js',
                 './public/scripts/aos.js',
+                './public/scripts/intlTelInput.min.js',
                 './public/scripts/call-candidate.js',
                 './public/scripts/about.js'],
-          dest: './public/scripts/_app.js',
+          dest: './public/dist/_app.js',
         },
+        css: {
+          src: ['./public/styles/normalize.css',
+                './public/styles/aos.css',
+                './public/styles/base.css'],
+          dest: './public/dist/_app.css',
+        },
+      },
+      watch: {
+          js: {
+              files: ['./public/scripts/**/*.js'],
+              tasks: ['concat:js'],
+          },
+          css: {
+              files: ['./public/styles/**/*.css'],
+              tasks: ['concat:css'],
+          },
       },
     })
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
 }
