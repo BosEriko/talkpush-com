da5e9e13953f92026eef2eafece5ea112f26cbcb
Bos Eriko <eriko.bos@talkpush.com>
2017-08-22 17:05:36 +0800

    Accelerate form is now working

--- public/dist/_app.js
+++ public/dist/_app.js
@@ -1393,15 +1393,16 @@ function submitData(c) {
 
 $(function() {
   var accelerateCampaignID = "3327"
-  var accelerateFormData = new FormData()
+  var accelerateFormData = new FormData();
   $('form.accelerate-form').submit(function(e) {
-    e.preventDefault
     accelerateFormData.append("api_key", apiKey);
     accelerateFormData.append("api_secret", apiSecret);
+    accelerateFormData.append("source", "Landing Page");
     accelerateFormData.append("campaign_invitation[first_name]", $("form.accelerate-form input[name='acc_firstName']").val());
     accelerateFormData.append("campaign_invitation[last_name]", $("form.accelerate-form input[name='acc_lastName']").val());
     accelerateFormData.append("campaign_invitation[email]", $("form.accelerate-form input[name='acc_email']").val());
     accelerateFormData.append("campaign_invitation[user_phone_number]", $("form.accelerate-form input[name='acc_phoneNumber']").val());
+    console.log(accelerateFormData)
     $.ajax({
       url: "https://" + host + "/api/talkpush_services/campaigns/" + accelerateCampaignID + "/campaign_invitations",
       type: "POST",
@@ -1422,9 +1423,12 @@ $(function() {
           console.log("Thank you for your interest.");
         }
       },
+      error: function(a, b, c) {
+      },
       xhrFields: {
         withCredentials: false
       }
     });
+    e.preventDefault();
   });
 });
