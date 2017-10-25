c30b6f0f97684f2d64b42b084eee2c624399dedc
Bos Eriko <eriko.bos@talkpush.com>
2017-10-25 20:30:16 +0800

    Now using Grunt to compile the sass.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,52 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
 
-    grunt.initConfig({
-      concat: {
-        js: {
-          src: ['./public/scripts/jquery-3.1.0.min.js',
-                './public/scripts/app.js',
-                './public/scripts/aos.js',
-                './public/scripts/typed.js',
-                './public/scripts/intlTelInput.min.js',
-                './public/scripts/call-candidate.js',
-                './public/scripts/accelerate-form.js'],
-          dest: './public/dist/_app.js',
-        },
-        css: {
-          src: ['./public/styles/normalize.css',
-                './public/styles/aos.css',
-                './public/styles/base.css'],
-          dest: './public/dist/_app.css',
-        },
-      },
-      watch: {
-          js: {
-              files: ['./public/scripts/**/*.js'],
-              tasks: ['concat:js'],
-          },
-          css: {
-              files: ['./public/styles/**/*.css'],
-              tasks: ['concat:css'],
-          },
-      },
-    })
+	grunt.initConfig({
+		concat: {
+			js: {
+				src: [
+					'./public/scripts/jquery-3.1.0.min.js',
+					'./public/scripts/app.js',
+					'./public/scripts/aos.js',
+					'./public/scripts/typed.js',
+					'./public/scripts/intlTelInput.min.js',
+					'./public/scripts/call-candidate.js',
+					'./public/scripts/accelerate-form.js'
+				],
+				dest: './public/dist/_app.js'
+			},
+			css: {
+				src: [
+					'./public/styles/normalize.css', './public/styles/aos.css', './public/styles/base.css'
+				],
+				dest: './public/dist/_app.css'
+			}
+		},
+		watch: {
+			js: {
+				files: ['./public/scripts/**/*.js'],
+				tasks: ['concat:js']
+			},
+			css: {
+				files: ['./public/styles/**/*.css'],
+				tasks: ['concat:css']
+			},
+			sass: {
+				files: ['./public/styles/**/*.sass'],
+				tasks: ['sass']
+			}
+		},
+		sass: {
+			dist: {
+				files: {
+					'./public/styles/base.css': './public/styles/base.sass'
+				}
+			}
+		}
+	})
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['concat', 'watch']);
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.registerTask('default', ['concat', 'watch', 'sass']);
 
 }
