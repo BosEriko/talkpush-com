778c0eaa75be22b0c5361cbd94d459e2c57cd522
Bos Eriko <bos.eriko@gmail.com>
2018-07-30 19:20:37 +0800

    fixed the mispelling

--- main.rb
+++ main.rb
@@ -65,7 +65,7 @@ post '/whatsapp-form', :provides => :json do
   Mail.deliver do
     to      ENV["TO_ADDRESS"]
     from    ENV["EMAIL_ADDRESS"]
-    subject "Whatsapp enquiry from #{full_name} at #{company}"
+    subject "Whatsapp inquiry from #{full_name} at #{company}"
     body    "Name: #{full_name}\nCompany: #{company}\nEmail: #{email}\nPhone: #{phone}\nExpected volumes: #{volumes}"
   end
   content_type :json
