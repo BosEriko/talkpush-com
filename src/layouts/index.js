f3496ac9d5242afe7efcbe9b07ca9c8d68ce9dee
Bos Eriko <eriko.bos@gmail.com>
2018-10-04 21:37:49 +0800

    more cleaner way for https redirect

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -27,6 +27,12 @@ const facebookLiveChatScript = () => {
   }
 }
 
+const httpsRedirect = () => {
+  if ((typeof window !== 'undefined') && (location.protocol != 'https:') && !(location.hostname === "localhost" || location.hostname === "127.0.0.1")) {
+    location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+  }
+}
+
 const Layout = (props) => (
   <div>
     <Helmet>
@@ -37,10 +43,10 @@ const Layout = (props) => (
       <meta property='og:type' content='website' />
       <meta property='og:image' content={props.data.site.siteMetadata.image} itemprop='image' />
       <script>{facebookLiveChatScript()}</script>
-      <script>if((location.protocol != 'https:') && !(location.hostname === "localhost" || location.hostname === "127.0.0.1")) location.href = 'https:' + window.location.href.substring(window.location.protocol.length)</script>
+      <script>{httpsRedirect()}</script>
     </Helmet>
     <Header title={props.data.site.siteMetadata.title} />
-    <div>{props.children({...props, ...{ siteTitle: props.data.site.siteMetadata.title }})}</div>
+    <div>{props.children({ ...props, ...{ siteTitle: props.data.site.siteMetadata.title } })}</div>
     <Footer title={props.data.site.siteMetadata.title} />
   </div>
 )
