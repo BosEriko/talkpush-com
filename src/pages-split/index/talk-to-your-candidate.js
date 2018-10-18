929f1f14cc0c2ef0128aa0fa77ce0a5a31e186a1
BosEriko <eriko.bos@gmail.com>
2018-10-18 11:28:35 +0800

    Test done

--- src/pages-split/index/talk-to-your-candidate.js
+++ src/pages-split/index/talk-to-your-candidate.js
@@ -5,7 +5,6 @@ import videoWallpaper from './videos/background.mp4'
 const calendlyPopUp = () => {
   Calendly.showPopupWidget('https://calendly.com/talkpush-demo/talkpush-intro-demo/');
 }
-test
 
 const TalkToYourCandidateIndexPageSplit = () => (
   <div className={styles.block}>
